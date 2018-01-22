448dd313f7767f29bcf03b10311f863e5a3a00cc
陈吴 <chenwu@chenwudeMacBook-Pro.local>
2018-01-22 14:26:19 +0800

    Initial commit

---
+++ app/src/main/java/com/example/chenwu/android/okhttp_test/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.chenwu.android.okhttp_test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextView= (TextView)findViewById(R.id.text1);
+    }
+
+    public String doGet(){
+        OkHttpClient okHttpClient
+
+
+
+        return null;
+
+    }
+}
